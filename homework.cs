8963a072889ade948a443ca69a63169fda40dfb4
X2X <vitenikpavel@gmail.com>
2026-02-28 02:49:41 +0300

    homework

---
+++ homework.cs
@@ -0,0 +1,31 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var name = Console.ReadLine();
+        var age = int.Parse(Console.ReadLine());
+
+        var p1 = new Person { Name = name, Age = age };
+        var p2 = ModifyPersonsName(p1);
+
+        Console.WriteLine($"{p1.Name}, {p1.Age}");
+        Console.WriteLine($"{p2.Name}, {p2.Age}");
+    }
+
+    static Person ModifyPersonsName(Person person)
+    {
+        return new Person 
+        { 
+            Name = $"[{person.Name}]",  
+            Age = person.Age            
+        };
+    }
+}
+
+class Person
+{
+    public string Name { get; set; } = string.Empty;
+    public int Age { get; set; }
+}
